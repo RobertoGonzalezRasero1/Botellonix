0696ac9f4410c2e76d9566b1610acc26cc3a6c8d
jbarrosgarcia <jose_bg_3@hotmail.com>
2017-03-19 20:11:53 +0100

    Creados los metodos para leer y crear el archivo

--- src/proyectoprogramacion/Metodos.java
+++ src/proyectoprogramacion/Metodos.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -78,5 +80,28 @@ public class Metodos {
         }
         
 }
+     public void engadir(String nomeFich, String dato){
+       
+            FileWriter fw = null;
+            PrintWriter esc = null;
+        try{    
+            fw = new FileWriter(nomeFich,true);
+            esc = new PrintWriter(fw);
+            esc.append("\n");
+            esc.append(dato);
+        } catch (IOException ex) {
+            System.out.println("erro engadir"+ ex.getMessage());
+        }
+        finally{
+                try {
+                    if(fw!=null)
+                        fw.close();
+                    if(esc!= null)
+                       esc.close();
+                } catch (IOException ex) {
+                    System.out.println("erro de peche"+ex.getMessage());
+                }
+        }            
+} 
     }
 
