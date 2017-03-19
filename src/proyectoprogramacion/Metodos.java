a2ee99d4cf07c978f92dec381a5546337fcf84fc
jbarrosgarcia <jose_bg_3@hotmail.com>
2017-03-19 20:08:36 +0100

    Añadiendo los metodos para ficheros y creando la interfaz grafica

--- src/proyectoprogramacion/Metodos.java
+++ src/proyectoprogramacion/Metodos.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,24 +24,25 @@ import java.util.logging.Logger;
  */
 public class Metodos {
 
-    //Creo un ArrayList de tipo Producto        
+    File f = new File("productox.txt");
+    Scanner sc = null;
 
+    //Creo un ArrayList de tipo Producto        
     ArrayList<Producto> item = new ArrayList<>();
-    Producto p=new Producto();
+    Producto p = new Producto();
 
     //Añado Items esto habra que meterlo en un bucle para que lo pregunte hasta que se le de a salir 
     public void addItem() {
-        p.setValores(JOptionPane.showInputDialog("Introduzca el nombre del producto"), Float.parseFloat(JOptionPane.showInputDialog("Introduzca el precio del producto")),Integer.parseInt("Introduzca la cantidad del producto"));
+        p.setValores(JOptionPane.showInputDialog("Introduzca el nombre del producto"), Float.parseFloat(JOptionPane.showInputDialog("Introduzca el precio del producto")), Integer.parseInt("Introduzca la cantidad del producto"));
         item.add(p);
     }
 
     //Vendemos un producto que tambien tendra que ir en un bucle 
-
     public void delItem() {
         item.removeAll(item);
-        
+
     }
-            //Este metodo muestra toda la lista de productos que es lo que se deveria sobrescribir en el fichero cadavez que se termina de 
+    //Este metodo muestra toda la lista de productos que es lo que se deveria sobrescribir en el fichero cadavez que se termina de 
     //trabajar con el
 
     public void viewItem() {
@@ -49,14 +51,32 @@ public class Metodos {
         }
 
     }
-    public void abrirFichero(){
-        File f = new File("productox.txt");
-        Scanner sc=null;
-        
+
+    public void abrirFichero() {
+
         try {
-            sc=new Scanner(f);
+            sc = new Scanner(f);
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public void escribirArchivo(String nomeFich){
+        File f = new File(nomeFich);
+        PrintWriter escribir= null;
+        try {
+            // abrimos ficheiro para escribir
+         escribir= new PrintWriter(f);
+         for(int i =0;i<4;i++)
+             escribir.println(i);           
+        } catch (FileNotFoundException ex) {
+            System.out.println("erro escritura"+ ex.getMessage());
+           }
+        finally{
+            escribir.close();
+        }
+        
 }
+    }
+
